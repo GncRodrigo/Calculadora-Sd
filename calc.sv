c46fa0e05820c285b7f193479ee5cba6ad852470
Gustavo Gallo <gustavotibollagallo@gmail.com>
2025-04-20 14:48:40 -0300

    atualizei o calc_top e o ctrl.sv, tao amarradinhos certo, no calc ainda nao ta cm a logica certa dos displays brother

--- calc.sv
+++ calc.sv
@@ -22,7 +22,7 @@ module calc (
     logic [26:0] regA, regB, regAux;
     logic [3:0]  operacao;
     logic [26:0] count;
-    logic [3:0] display_counter;                // guarda em qual display tá
+   
 
     // Bloco sequencial: atualização do estado
     always_ff @(posedge clock or posedge reset) begin
@@ -37,14 +37,13 @@ module calc (
     always_ff @(posedge clock or posedge reset) begin
         if (reset) begin        // reset zera tudo, evita de ficar lixo
             digits   <= 0;
-            display_counter <= 0;
             regA     <= 0;
             regB     <= 0;
             regAux   <= 0;
             count    <= 0;
             status   <= 2'b11;   // como o status 00 significa erro, 01 ocupado, e 10 pronto. Assumi que 11 não significa nada
             operacao <= 0;
-        end else begin
+'        end else begin
             case (EA)
 
                 ESPERA_A: begin
@@ -52,28 +51,15 @@ module calc (
                     if (cmd <= 4'd9) begin
                         digits <= (digits * 10) + cmd; // faz o deslocamento e adiciona
 
-                        pos <= display_counter; // coloca pos, que ta ligado com o pos do ctrl na posição atual
-                        data <= cmd;            // coloca a entrada (numero) em data (dig em ctrl) 
-                        display_counter <= display_counter + 1; // passa pra proxima posição
-
+                       
                     end else if (cmd == 4'b1111) begin
                         digits <= digits / 10; // aqui ta rolando o backspace
-                        data <= 4'b0;          // apaga o atual
-                        display_counter <= display_counter - 1; // volta um espaço
-                        pos <= display_counter;                 // att pos
+                     
                     end else begin
                         regA <= digits; // salva no regA
                         digits <= 0; // zera o digits e consequentemente o display
-                        display_counter <= 0; // zera a posição antes de ir para ESPERA_B
-                        data[0] <= 0; // limpa os displays ao sair de ESPERA_A
-                        data[1] <= 0; 
-                        data[2] <= 0; 
-                        data[3] <= 0; 
-                        data[4] <= 0; 
-                        data[5] <= 0; 
-                        data[6] <= 0; 
-                        data[7] <= 0; 
-
+                       
+                      
                     end
                 end
 
@@ -85,37 +71,23 @@ module calc (
                     if (cmd <= 4'd9) begin
                         digits <= (digits * 10) + cmd; // Adiciona o novo dígito
 
-                        pos <= display_counter;
-                        data <= cmd;
-                        display_counter <= display_counter + 1;
 
                     end else if (cmd == 4'b1111) begin
                         digits <= digits / 10; // Remove o último dígito
 
-                        data <= 4'b0;
-                        display_counter <= display_counter - 1;
-                        pos <= display_counter;
+                
 
                     end else begin
                         regB <= digits; // Salva o valor em regB
                         digits <= 0;
-                        display_counter = 0;
-                        data[0] <= 0; // limpa os displays ao sair de ESPERA_B
-                        data[1] <= 0; 
-                        data[2] <= 0; 
-                        data[3] <= 0; 
-                        data[4] <= 0; 
-                        data[5] <= 0; 
-                        data[6] <= 0; 
-                        data[7] <= 0; 
-                        pos <= display_counter; // coloca pos no inicio para os resultados
+                        
                     end
                 end
 
                 RESULT: begin
                     case (operacao)
-                        4'b1010: digits <= regA + regB; status <= 2'b10; // soma // status pronto //não precisa de begin e end?, usa mais de uma linha
-                        4'b1011: digits <= regA - regB; status <= 2'b10; // subtração // status pronto
+                        4'b1010: begin digits <= regA + regB; status <= 2'b10; end // soma // status pronto 
+                        4'b1011: begin digits <= regA - regB; status <= 2'b10; end // subtração // status pronto
                         4'b1100: begin // multiplicação por somas sucessivas
                             if (status != 2'b01) begin
                                 digits <= 0;
@@ -132,14 +104,7 @@ module calc (
                     endcase
 
                     if (status == 2'b10) begin // se ta pronto, mostra o resultado, fazer em mais ciclos fica menor mas gasta mais ciclos
-                        data[0] <= digits[3:0]; 
-                        data[1] <= digits[7:4]; 
-                        data[2] <= digits[11:8]; 
-                        data[3] <= digits[15:12]; 
-                        data[4] <= digits[19:16]; 
-                        data[5] <= digits[23:20]; 
-                        data[6] <= digits[27:24]; 
-                        data[7] <= 0;
+                    
                     end 
                 end
 
--- calc_top.sv
+++ calc_top.sv
@@ -2,15 +2,16 @@ module calc_top(
     input logic [3:0] cmd,
     input logic reset,
     input logic clock
- 
+    // aqui nesses displays sera possivel verificar os numeros da calc
+    output logic [7:0] displays [6:0];
+    output logic [1:0] status;
 );
 
 logic [3:0] data;
 logic [3:0] pos;
-logic [1:0] status;
 
 calc calculadera(.clock(clock), .reset(reset), .cmd(cmd), .status(status) ,.data(data), .pos(pos));
 
-ctrl controladoro(.clock(clock), .reset(reset), .dig(data), .pos(pos));
+ctrl controladoro(.clock(clock), .reset(reset), .dig(data), .pos(pos), .segments(displays));
 
 endmodule
