c9f6a67b6e110e4f2bea3b16a54680b6755768b5
Gustavo Gallo <gustavotibollagallo@gmail.com>
2025-04-23 15:01:07 -0300

    mais

--- display.sv
+++ display.sv
@@ -13,17 +13,17 @@ module display(
   // "f" é a função que mapeia as entras em saídas. Neste caso,
   // é declarado como um vetor de 8 posições (8 segmentos do display), contendo
   // 8 bits cada (um para cada possibilidade de dígito a ser mostrado)
-  logic[7:0]ff[0:9]= {
-    8'b1111110,  // 0
-    8'b1100000,  // 1
-    8'b1101101,  // 2
-    8'b1111001,  // 3
-    8'b0110011,  // 4
-    8'b1011011,  // 5
-    8'b0011111,  // 6
-    8'b1110000,  // 7
-    8'b1111111,  // 8
-    8'b1110011   // 9
+  logic[6:0]ff[0:9]= {
+    7'b1111110,  // 0
+    7'b1100000,  // 1
+    7'b1101101,  // 2
+    7'b1111001,  // 3
+    7'b0110011,  // 4
+    7'b1011011,  // 5
+    7'b0011111,  // 6
+    7'b1110000,  // 7
+    7'b1111111,  // 8
+    7'b1110011   // 9
   };
 
   // conecta a saída ao vetor f, utilizando o índice para selecionar
