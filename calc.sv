8f07d251a5a2a878660fd0b906a11a5ea082972c
Lucas G <lucasgomes062005@gmail.com>
2025-04-24 16:48:44 -0300

    vezes

--- calc.sv
+++ calc.sv
@@ -141,21 +141,20 @@ module calc (
                 end
 
             endcase
-                if(!enable)begin
-                    enable <= 1;
-                end
+                
                 //LÓGICA PARA OS DISPLAYS
                // MEXEDOR DA POSIÇÃO
-               if (enable) begin
-                 if (pos > 4'b0111) begin
+                if (pos > 4'b0111) begin
                  // Reseta pos após todos os displays serem atualizados
                         pos <= 4'b0000;
                         status <= 2'b10;
-                 end else begin
+                end else begin
                 
                 if (status == 00 || (status == 2'b01 && operacao != 4'b1100)) begin
             
-                if(pos == 0)begin temp <= digits;end
+                if(pos == 0) begin 
+                    temp = digits; 
+                end
                 // mapeia para o values o que estiver no digits, tudo isso combinacionalmente
  
                 values[pos] <= temp % 10; temp <= temp/10; 
@@ -164,11 +163,9 @@ module calc (
                  data <= values[pos];
                 // Incrementa pos enquanto ocupado
                     pos <= pos + 1;
-                end
+                    end
                  end
                 end
-
-                end 
         end
 
     // mudar as maquina de estados
--- tb_calc_top.sv
+++ tb_calc_top.sv
@@ -36,7 +36,7 @@ module tb_calc_top;
 
   cmd = 4'd1; #20;
   cmd = 4'd2; #20;
-  cmd = 4'b1010; #20;
+  cmd = 4'b1100; #20;
   cmd = 4'd3; #20;
   cmd = 4'b1110; #20;
   #1000;
